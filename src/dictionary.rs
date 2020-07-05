99ae219b9ce9fefa27013903a0075b3a292bc0a6
paul <bares.paul@gmail.com>
2020-07-05 14:44:24 +0400

    clean

--- src/dictionary.rs
+++ src/dictionary.rs
@@ -1,26 +1,30 @@
+#![crate_name = "doc"]
+
 use std::collections::hash_map::DefaultHasher;
 use std::hash::{Hash, Hasher};
 
 static FREE_SLOT: i64 = -1;
 
 #[derive(Debug)]
-struct Dictionary {
+pub struct Dictionary {
     capacity: u64,
     size: u64,
-    hashTable: Vec<i64>,
+    hash_table: Vec<i64>,
     keys: Vec<i32>, // FIXME make it generic
 }
 
 impl Dictionary {
-    fn new(initialCapacity: u64) -> Dictionary {
+    /// Constructs a new empty dictionary of size 0 with the given initial capacity.
+    fn new(initial_capacity: u64) -> Dictionary {
         Dictionary {
-            capacity: initialCapacity,
+            capacity: initial_capacity,
             size: 0,
-            hashTable: vec![FREE_SLOT; initialCapacity as usize],
-            keys: vec![-1; initialCapacity as usize],
+            hash_table: vec![FREE_SLOT; initial_capacity as usize],
+            keys: vec![0; initial_capacity as usize],
         }
     }
 
+    /// Resizes the hash table to the given capacity by re-hashing all of the keys.
     fn resize(&mut self, new_capacity: u64) {
         let mut tmp = Dictionary::new(new_capacity);
 
@@ -32,73 +36,102 @@ impl Dictionary {
         }
 
         self.keys = tmp.keys;
-        self.hashTable = tmp.hashTable;
+        self.hash_table = tmp.hash_table;
         self.size = tmp.size;
         self.capacity = tmp.capacity;
     }
 
+    /// Computes the hash of the given key.
     fn hash(&self, key: i32) -> u64 {
         let mut s = DefaultHasher::default();
         key.hash(&mut s);
         s.finish() & (self.capacity - 1)
     }
 
-    fn map(&mut self, key: i32) -> u64 {
-        println!("=====================");
+    /// Inserts the key into the dictionary and returns its position. If the key is already in the
+    /// dictionary, it simply returns its position.
+    ///
+    /// # Example:
+    /// FIXME this code does not compile because the import but I don't know why
+    /// ```ignore
+    /// use dictionary::Dictionary;
+    ///
+    /// let mut dictionary = Dictionary::new(4);
+    /// assert_eq!(dictionary.map(5), 0);
+    /// assert_eq!(dictionary.map(5), 0);
+    /// ```
+    pub fn map(&mut self, key: i32) -> u64 {
         // Double the table size if 50% full
         if self.size >= (self.capacity >> 1) {
-            println!("resize !!!!!");
             self.resize(self.capacity << 1); // TODO make sure it does not overflow
-            println!("resize done: {:?}", self);
         }
 
         let mut index = self.hash(key);
-        loop {
-            println!("computed hash: {}, size: {}, capacity: {}", index, self.size, self.capacity);
-            let address = &self.hashTable[index as usize];
-            println!("address: {}", address);
-            if *address != FREE_SLOT {
-                let k = match self.keys.get(*address as usize) {
-                    None => { break; }
-                    Some(v) => { v }
-                };
-
-                if *k == key { // FIXME equals when generic
-                    return *address as u64;
+        while let Some(address) = self.hash_table.get(index as usize) {
+            match self.keys.get(*address as usize) {
+                None => { break; }
+                Some(k) => {
+                    if *k == key { // FIXME equals when generic
+                        return *address as u64;
+                    }
+                    index = (index + 1) & (self.capacity - 1); // linear probing, find a new available slot
                 }
-                index = (index + 1) & (self.capacity - 1); // linear probing, find a new available slot
-                continue;
-            }
-            break;
-        };
-
-        let newAddress = self.size;
-        println!("setting key: {}, at address: {}", key, newAddress);
-        self.keys[newAddress as usize] = key;
-        println!("size of keys: {}", self.keys.len());
-        self.hashTable[index as usize] = newAddress as i64;
-        self.size += 1;
-        newAddress
+            };
+        }
+        if let Some(pos) = self.get(key) {
+            pos as u64
+        } else {
+            let new_address = self.size;
+            println!("setting key: {}, at address: {}", key, new_address);
+            self.keys[new_address as usize] = key;
+            println!("size of keys: {}", self.keys.len());
+            self.hash_table[index as usize] = new_address as i64;
+            self.size += 1;
+            new_address
+        }
     }
 
-    fn read(&self, position: u64) -> i32 {
+    /// Reads the key stored at a given dictionary position or return -1 if there is no key stored
+    /// at the given position.
+    pub fn read(&self, position: u64) -> i32 {
         match self.keys.get(position as usize) {
             None => { FREE_SLOT as i32 }
             Some(v) => { *v }
         }
     }
 
-    fn get_position(&self, key: i32) {
-        // TODO
+    /// Gets the position of the key in the dictionary or return -1 if there is no key at this
+    /// position.
+    pub fn get_position(&self, key: i32) -> i64 {
+        match self.get(key) {
+            None => { FREE_SLOT },
+            Some(pos) => { pos as i64 },
+        }
+    }
+
+    fn get(&self, key: i32) -> Option<i64> {
+        let mut index = self.hash(key);
+        while let Some(address) = self.hash_table.get(index as usize) {
+            match self.keys.get(*address as usize) {
+                None => { break; }
+                Some(k) => {
+                    if *k == key { // FIXME equals when generic
+                        return Some(*address);
+                    }
+                    index = (index + 1) & (self.capacity - 1); // linear probing, find a new available slot
+                }
+            };
+        }
+        None
     }
 }
 
 #[cfg(test)]
 mod tests {
-    use crate::dictionary::Dictionary;
+    use crate::dictionary::{Dictionary, FREE_SLOT};
 
     #[test]
-    fn test() {
+    fn testWithInteger() {
         let mut dictionary = Dictionary::new(4);
         assert_eq!(dictionary.map(5), 0);
         assert_eq!(dictionary.map(0), 1);
@@ -107,5 +140,15 @@ mod tests {
         assert_eq!(dictionary.map(5), 0); // key already exists
         assert_eq!(dictionary.map(3), 4);
         assert_eq!(dictionary.map(1), 5);
+
+        assert_eq!(dictionary.get_position(5), 0);
+        assert_eq!(dictionary.get_position(0), 1);
+        assert_eq!(dictionary.get_position(2), 2);
+        assert_eq!(dictionary.get_position(1234), 3);
+        assert_eq!(dictionary.get_position(3), 4);
+        assert_eq!(dictionary.get_position(1), 5);
+        assert_eq!(dictionary.get_position(11111), FREE_SLOT); // does not exist
     }
+
+    // TODO test with struct
 }
